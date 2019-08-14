f64a5a7971e9f2c0e3942d117a2f6c23a45d11c3
Davidoff <jujus.shen@126.com>
2019-08-14 15:37:10 +0800

    - 并发演示代码

--- demo-boot/src/test/java/org/coodex/concrete/demo/boot/ParallelDemo.java
+++ demo-boot/src/test/java/org/coodex/concrete/demo/boot/ParallelDemo.java
@@ -17,7 +17,6 @@
 package org.coodex.concrete.demo.boot;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.parser.Feature;
 import org.coodex.concurrent.ExecutorsHelper;
 import org.coodex.concurrent.Parallel;
 import org.coodex.util.Clock;
